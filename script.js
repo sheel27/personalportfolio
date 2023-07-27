6a04662175f98f9ccd0c4d5b839a7683a3fe71b5
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-07-27 21:38:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,15 +2,28 @@ function Menu(e) {
   let list = document.querySelector("ul");
   let nav = document.querySelector("nav");
 
-  if (e.name === "menu") {
-    e.name = "close";
+//   if (e.name === "menu") {
+//     e.name = "close";
+//     list.classList.add("top-[80px]");
+//     list.classList.add("opacity-100");
+//     list.classList.add("bg-gray-200"); 
+//   } else {
+//     e.name = "menu";
+//     list.classList.remove("top-[80px]");
+//     list.classList.remove("opacity-100");
+//     list.classList.remove("bg-gray-200"); 
+//   }
+// }
+
+ if (e.getAttribute("name") === "menu") {
+    e.setAttribute("name", "close");
     list.classList.add("top-[80px]");
     list.classList.add("opacity-100");
     list.classList.add("bg-gray-200"); 
   } else {
-    e.name = "menu";
+    e.setAttribute("name", "menu");
     list.classList.remove("top-[80px]");
     list.classList.remove("opacity-100");
-    list.classList.remove("bg-gray-200"); 
+    list.classList.remove("bg-gray-200");
   }
 }
