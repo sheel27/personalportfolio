3dd0ff913a51ba5664e7c0d2f3363bc55f835a87
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-07-27 23:17:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-const navLinks = document.querySelector('.nav-links')
-function onToggleMenu(e){
-    e.name = e.name === 'menu' ? 'close' : 'menu'
-    navLinks.classList.toggle('top[9%]')
+function Menu(e) {
+  let list = document.querySelector("ul");
+  let nav = document.querySelector("nav");
+
+  if (e.name === "menu") {
+    e.name = "close";
+    list.classList.add("top-[80px]");
+    list.classList.add("opacity-100");
+    list.classList.add("bg-gray-200"); // Add the Tailwind CSS class for background color
+  } else {
+    e.name = "menu";
+    list.classList.remove("top-[80px]");
+    list.classList.remove("opacity-100");
+    list.classList.remove("bg-gray-200"); // Remove the Tailwind CSS class for background color
+  }
 }
