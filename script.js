07c789eaa6c383f4cb1990e094eeed786fad5b4c
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-07-29 13:43:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-function Menu(e) {
+function Menu() {
   let list = document.querySelector("ul");
   let nav = document.querySelector("nav");
 
-  if (e.name === "menu") {
-    e.name = "close";
+  if (name === "menu") {
+    name = "close";
     list.classList.add("top-[80px]");
     list.classList.add("opacity-100");
     list.classList.add("bg-gray-200");
   } else {
-    e.name = "menu";
+    name = "menu";
     list.classList.remove("top-[80px]");
     list.classList.remove("opacity-100");
     list.classList.remove("bg-gray-200"); 
