c26130729cea37493620e7c703a903bb9ec048ea
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-07-29 15:12:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,14 @@
-function menu(e) {
+function Menu(e) {
   let list = document.querySelector("ul");
-  let nav = document.querySelector("nav");
 
-  if (e.name === "menu") {
-    e.name = "close";
-    list.classList.add("top-[80px]");
-    list.classList.add("opacity-100");
-    list.classList.add("bg-gray-200");
-  } else {
-    e.name = "menu";
+  e.name === "menu" ?
+    (e.name = "close",
+    list.classList.add("top-[80px]"),
+    list.classList.add("opacity-100"),
+    list.classList.add("bg-gray-200"))
+  :
+    (e.name = "menu",
     list.classList.remove("top-[80px]");
-    list.classList.remove("opacity-100");
-    list.classList.remove("bg-gray-200"); 
+    list.classList.remove("opacity-100"),
+    list.classList.remove("bg-gray-200")) 
   }
-}
