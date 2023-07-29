b86d48bc16810cd496f26f5da4d65e2a53963c1e
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2023-07-29 16:02:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,3 @@
-function Menu(e) {
-  let list = document.querySelector("ul");
-
-  e.name === "menu" ?
-    (e.name = "close",
-    list.classList.add("top-[80px]"),
-    list.classList.add("opacity-100"),
-    list.classList.add("bg-gray-200"))
-  :
-    (e.name = "menu",
-    list.classList.remove("top-[80px]");
-    list.classList.remove("opacity-100"),
-    list.classList.remove("bg-gray-200")) 
-  }
+$('.icon').click(function(){
+  $('span').toggleClass("cancel");
+});
